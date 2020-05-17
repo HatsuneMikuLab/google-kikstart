70225dfa2d5cd184684cb18f075b6b1b74c82f5e
Pavlov Oleh <opavlov@conceptsoft.io>
2020-05-17 18:47:51 +0900

    boilerplate

---
+++ boilerplate.js
@@ -0,0 +1,29 @@
+const io = require('readline')
+    .createInterface(process.stdin);
+
+const readln = () => new Promise(
+    res => io.question('>', answer => res(answer))
+);
+
+const formatSolution = async (solution = Function(), argsLn = 1) => {
+    try {
+        const T = Number(await readln());
+        console.log('T: ', T)
+        let Ti = 0;
+        while (++Ti <= T) {
+            const args = [];
+            for (let i = 0; i < argsLn; ++i) {
+                args.push(await readln())
+            }
+            console.log(args)
+            const result = solution(...args);
+            console.log('Case #%d: %d', Ti, result)
+        }
+        process.exit(0)
+    } catch (error) {
+        console.log('ERROR: ', error)
+    }
+}
+
+formatSolution((a, b) => Number(a) + Number(b), 2)
+
